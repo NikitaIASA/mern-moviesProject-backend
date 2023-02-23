3d97cc618a9f86aaa079ee8ab88ac1a784f29827
Nikita Chaika <iasastudent2021@gmail.com>
2023-02-23 02:40:53 +0200

    resetting and changing the user password

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,8 @@ const User = require('../models/user')
 const EmailVerificationToken = require('../models/emailVerificationToken');
 const { isValidObjectId } = require('mongoose');
 const { generateOTP, generateMailTransporter } = require('../utils/mail');
-const { sendError } = require('../utils/helper');
+const { sendError, generateRandomByte } = require('../utils/helper');
+const PasswordResetToken = require('../models/passwordResetToken');
 
 exports.create = async (req, res) => {
   const { name, email, password } = req.body
@@ -153,3 +154,41 @@ exports.forgetPassword = async (req, res) => {
 
   res.json({ message: "Link sent to your email!" });
 };
+
+exports.sendResetPasswordTokenStatus = (req, res) => {
+  res.json({ valid: true })
+}
+
+exports.resetPassword = async (req, res) => {
+  const { newPassword, userId } = req.body;
+
+  const user = await User.findById(userId);
+  const matched = await user.comparePassword(newPassword);
+  if (matched)
+    return sendError(
+      res,
+      "The new password must be different from the old one!"
+    );
+
+  user.password = newPassword;
+  await user.save();
+
+  await PasswordResetToken.findByIdAndDelete(req.resetToken._id);
+
+  const transport = generateMailTransporter();
+
+  transport.sendMail({
+    from: "security@reviewapp.com",
+    to: user.email,
+    subject: "Password Reset Successfully",
+    html: `
+      <h1>Password Reset Successfully</h1>
+      <p>Now you can use new password.</p>
+
+    `,
+  });
+
+  res.json({
+    message: "Password reset successfully, now you can use new password.",
+  });
+}
